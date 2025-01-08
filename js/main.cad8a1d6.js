f7d634bfa30ea5875ffb72186309d8ec3b0ff4a2
congdt <congdt.kdt@gmail.com>
2025-01-08 21:50:33 +0700

    finish

--- js/main.cad8a1d6.js
+++ js/main.cad8a1d6.js
@@ -28654,7 +28654,7 @@
                     })
                 }), (0,
                     Ll.jsx)("a", {
-                        href: "https://aigentfi.com/",
+                        href: "link_aigentfi",
                         target: "_blank",
                         rel: "noopener noreferrer",
                         children: (0,
